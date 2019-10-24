0974bd84d1d8c1a2f19c184dbdb1f075280222ea
yk272 <yadukrishnan272@gmail.com>
2019-10-24 18:45:16 +0530

    first commit

---
+++ src/main/java/io/digiledge/springboottutorial/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package io.digiledge.springboottutorial.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String> {
+	
+	
+}
